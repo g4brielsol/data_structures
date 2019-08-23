4cf456259ce7605ddf570c98e06cb5531707d5ec
Gabriel Sol <gabsolsilva@gmail.com>
2019-08-23 13:24:37 -0300

    projeto terminado

--- criptografia2.c
+++ criptografia2.c
@@ -19,17 +19,7 @@ Lista* inserir(Lista* proximo, char informacao)
  return novo;
 }
 
-Lista* inserir_qqr_posicao(Lista* lista, Lista* no)
-{
-    Lista* ponteiro;
-    ponteiro = (Lista*) malloc(sizeof(Lista));
-    ponteiro->info = no;
-    ponteiro->prox = lista->prox;
-    lista->prox = ponteiro;
-    return ponteiro;
-}
-
-// percorrer: percorre os elementos
+// percorrer: percorre a lista printando os elementos
 void percorrer(Lista* list)
 {
     Lista* p = list;
@@ -52,38 +42,6 @@ int deslocamento(Lista* list)
     
     deslocamento = deslocamento % 26;
     return deslocamento;
-}
-
-
-// retira: retira elemento da lista */
-Lista* retirar(Lista* lista, char caractere_retirado) 
-{
-    Lista* ant = NULL; /* ponteiro para elemento anterior */
-    Lista* ponteiro = lista; /* ponteiro para percorrer a lista*/
-    /* procura elemento na lista, guardando anterior */
-    while (ponteiro != NULL && ponteiro->info != caractere_retirado)
-    {
-        ant = ponteiro;
-        ponteiro = ponteiro->prox;
-    }
-    /* verifica se achou elemento */
-    if (ponteiro == NULL)
-    {
-        return lista; /* não achou: retorna lista original */
-    }
-    /* retira elemento */
-    if (ant == NULL) 
-    {
-        //retira elemento do inicio 
-        lista = ponteiro->prox;
-    }
-    else 
-    {
-        // retira elemento do meio da lista
-        ant->prox = ponteiro->prox;
-    }
-    free(ponteiro);
-    return lista;
 }
 
 // faz um free em todos os nos da lista encadeada
@@ -111,8 +69,20 @@ void main()
     char chave_simetrica[50];
     scanf("%s", &chave_simetrica);
     int i;
-
     
+    Lista* lista_mensagem = (Lista*) malloc(sizeof(Lista));
+    // insere o ultimo caractere da variavel mensagem na lista
+    lista_mensagem->info = mensagem[strlen(mensagem) - 1];
+    // ultima posicao na lista aponta para nulo
+    lista_mensagem->prox = NULL;
+    //adiciona os caracteres de tras para frente, para a lista ficar em ordem
+    for(i=strlen(mensagem) - 2; i >= 0; i--)
+    {
+        //printf("%c", mensagem[i]);
+        lista_mensagem = inserir(lista_mensagem, mensagem[i]);
+    }
+    
+    //faz a lista encadeada com a chave
     Lista* lista_chave_simetrica = (Lista*) malloc(sizeof(Lista));
     lista_chave_simetrica->info = chave_simetrica[strlen(chave_simetrica) - 1]; 
     lista_chave_simetrica->prox = NULL;
@@ -122,81 +92,117 @@ void main()
         lista_chave_simetrica = inserir(lista_chave_simetrica, chave_simetrica[i]);
         tamanho_chave += 1;
     }
-    //percorrer(lista_chave_simetrica);
-    int j =0;
+
+    //cria array com intervalos
     int intervalos[tamanho_chave];
     int desloc;
+    //calcula o deslocamento
     desloc = deslocamento(lista_chave_simetrica);
-    Lista* copia = (Lista*) malloc(sizeof(Lista));
-    copia = lista_chave_simetrica;
-    //printf("%d\n", tamanho_chave);
-   //percorrer(copia);
 
-   while(copia != NULL)
-  {
-      intervalos[j] = (copia->info - 97) ^ desloc;
-      copia = copia->prox;
+    // copia o inicio da lista encadeada da chave
+    Lista* copia_chave = (Lista*) malloc(sizeof(Lista));
+    copia_chave = lista_chave_simetrica;
+    int j =0;
+    // calcular intervalos encriptacao ou decriptacao
+    while(copia_chave != NULL)
+    {
+      intervalos[j] = (copia_chave->info - 97) ^ desloc;
+      copia_chave = copia_chave->prox;
       j += 1; 
-  }
-  /*
-  for(j=0; j<tamanho_chave; j++)
-  {
-      printf("%d\n", intervalos[j]);
-  }
-   */
-    Lista* lista_mensagem = (Lista*) malloc(sizeof(Lista));
-    // insere o ultimo caractere da variavel mensagem
-    lista_mensagem->info = mensagem[strlen(mensagem) - 1];
-    lista_mensagem->prox = NULL;
-    // aponta para o null, fim da lista
-    int tamanho_mensagem = 0;
-    //adiciona os caracteres de tras para frente, para a lista ficar em ordem
-    for(i=strlen(mensagem) - 2; i >= 0; i--)
-    {
-        //printf("%c", mensagem[i]);
-        lista_mensagem = inserir(lista_mensagem, mensagem[i]);
-        tamanho_mensagem += 1;
     }
-    //percorrer(lista_mensagem);
-    //Lista* copia_2 = lista_chave_simetrica; //(Lista*) malloc(sizeof(Lista));
-    //copia_2 = lista_chave_simetrica;
+
+    // se for para fazer a criptografia
+    if(cripto == 0)
+    {
     
-    //Lista* copia_lista_mensagem = lista_mensagem; //(Lista*) malloc(sizeof(Lista));
-    //copia_lista_mensagem = lista_mensagem; 
-    int posicao=0, contador_nos=1;
-    Lista* copia_mens = lista_mensagem;
-
-    while(lista_mensagem != NULL)
-    {
-        lista_mensagem = lista_mensagem->prox;
-        contador_nos += 1;
-        //printf("%d", intervalos[posicao]);
-        if(contador_nos == intervalos[posicao])
+        int posicao=0, contador_nos=1;
+        Lista* copia_mens = lista_mensagem;
+        // insere nos da chave na mensagem
+        while(lista_mensagem != NULL)
         {
-            //lista_mensagem = inserir_qqr_posicao(lista_mensagem, lista_chave_simetrica);
-            //lista_chave_simetrica = lista_chave_simetrica->prox;
-            
-            Lista* ponteiro = (Lista*) malloc(sizeof(Lista));
-            ponteiro->info = lista_chave_simetrica->info;
-            ponteiro->prox = lista_mensagem->prox;
-            lista_mensagem->prox = ponteiro;
-            lista_chave_simetrica = lista_chave_simetrica->prox;
-            posicao += 1;
-            contador_nos = -1;
+            lista_mensagem = lista_mensagem->prox;
+            contador_nos += 1;
+            if(contador_nos == intervalos[posicao])
+            {
+                Lista* ponteiro = (Lista*) malloc(sizeof(Lista));
+                ponteiro->info = lista_chave_simetrica->info;
+                ponteiro->prox = lista_mensagem->prox;
+                lista_mensagem->prox = ponteiro;
+                lista_chave_simetrica = lista_chave_simetrica->prox;
+                posicao += 1;
+                contador_nos = -1;
+            }
         }
+
+        // aplicacao da cifra de cesar: mudar as letras da mensagem + chave para finalizar a criptografia
+        Lista* ponteiro_deslocamento_inicial = copia_mens;
+        int tamanho_crypto = 0;
+        while(copia_mens != NULL)
+        {
+            copia_mens->info = (((copia_mens->info - 97) + desloc) % 26) + 97;
+            copia_mens = copia_mens->prox;
+            desloc += 1;
+            tamanho_crypto += 1;
+        }
+        // printa a saida da encriptacao
+        printf("%d\n", strlen(mensagem));
+        printf("%d\n", tamanho_crypto);
+        percorrer(ponteiro_deslocamento_inicial);
+        // da um free nos espacos de memoria alocados dinamicamente
+        liberar(copia_mens);
+        liberar(ponteiro_deslocamento_inicial);
     }
-    //percorrer(copia_mens);
-    Lista* ponteiro_deslocamento = (Lista*) malloc(sizeof(Lista));
-    Lista* ponteiro_deslocamento_inicial = copia_mens;
-    while(copia_mens != NULL)
+
+    // se for para decriptar
+    else
     {
-        copia_mens->info = (((copia_mens->info - 97) + desloc) % 26) + 97;
-        copia_mens = copia_mens->prox;
-        desloc += 1;
-        //ponteiro_deslocamento = ponteiro_deslocamento->prox; 
+        //aplicacao da cifra de cesar de modo reverso para decifrar a mensagem
+        int aux_desloc = desloc;
+        Lista* copia_mensagem = lista_mensagem;
+        while(copia_mensagem != NULL)
+        {
+            int caractere = (copia_mensagem->info - 97) - (aux_desloc % 26);
+            if (caractere < 0)
+            {
+                copia_mensagem->info = caractere + 26 + 97;
+            }
+            else
+            {
+                copia_mensagem->info = caractere + 97;
+            }
+            aux_desloc += 1;
+            copia_mensagem = copia_mensagem->prox;
+            
+        }
+        //remocao dos nos da chave que estao na mensagem original
+        int posicao=0, contador_nos=1, tamanho_decrypto=0;
+        Lista* copia_mens = lista_mensagem;
+        while(copia_mens != NULL)
+        {
+            contador_nos += 1;
+            copia_mens = copia_mens->prox; 
+            if(contador_nos == intervalos[posicao])
+            {
+                Lista* atual = NULL; 
+                atual = copia_mens;
+                copia_mens = copia_mens->prox;
+                atual->prox = copia_mens->prox;
+                contador_nos = 0;
+                posicao += 1;
+            }
+            tamanho_decrypto += 1;
+        }
+        // printa a saida para a mensagem decifrada
+        printf("%d\n", strlen(mensagem));
+        printf("%d\n", tamanho_decrypto);
+        percorrer(lista_mensagem);
+        // da um free nos espacos de memoria alocados dinamicamente
+        liberar(copia_mensagem);
+        liberar(copia_mens);        
     }
-    percorrer(ponteiro_deslocamento_inicial);
-
-
+    // da um free nos espacos de memoria alocados dinamicamente
+    liberar(lista_mensagem);
+    liberar(lista_chave_simetrica);
+    liberar(copia_chave);
 }
     
